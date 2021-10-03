630f6d8d80946820c45b882c683758389905af9d
leonhwb <972744806@qq.com>
2021-10-03 22:47:57 +0800

    相对导入

--- s2s_dataloader/template.py
+++ s2s_dataloader/template.py
@@ -1,4 +1,4 @@
-from .s2s_download import RealTimeGribFileConfig, Param
+from .s2s_download import RealTimeGribFileConfig, Param, ReforecastGrib_OnTheFly_FileConfig
 import os
 from typing import List
 import xarray as xr
@@ -97,9 +97,9 @@ class S2SRealtimeBase:
             return ens
 
 
-class S2SReforecastBase:
+class S2SOnTheFlyReforecastBase:
     """
-    读取S2S数据的回算资料
+    读取S2S数据的回算资料OnTheFly模式
     在该类中，所有关于要素的查询使用以下方式：
         字符串：parameter + level
         例如：
@@ -108,15 +108,66 @@ class S2SReforecastBase:
             输入的parameter+level字符串会被解码处parameter和level信息，解码方式参照Param.separate_FactorLevel
     """
 
-    def __init__(self, data_center: Param.data_center, data_type: Param.data_type):
-        self.data_type = data_type
-        self.data_center = data_center
+    def __init__(self, data_center: Param.data_center, ):
+        self.__data_center = data_center
+        assert data_center in ReforecastGrib_OnTheFly_FileConfig.data_center, f'{data_center}不在{Param.data_center}内'
 
-    def init_date_range(self, *parameter_levels) -> List[str]:
-        pass
+    @property
+    def ensNums(self) -> int:
+        return ReforecastGrib_OnTheFly_FileConfig.pfEnsNums[self.__data_center] + 1  # 扰动成员数 + 控制成员数
 
-    def load(self, parameter_level: str, init_date: str) -> xr.DataArray:
-        pass
+    @property
+    def data_center(self) -> str:
+        return self.__data_center  # 字符串是不可变类型，安全
+
+    def cal_date_range(self, *parameter_levels, number: int = 0) -> List[str]:
+        ans = {}
+        for par_ll in parameter_levels:
+            parameter, level = Param.separate_FactorLevel(par_ll)
+            _period = ReforecastGrib_OnTheFly_FileConfig.init_date_range(data_center=self.__data_center,
+                                                                         parameter=parameter,
+                                                                         number=number,
+                                                                         level=level)
+            ans = ans.intersection(set(_period)) if ans != {} else set(_period)
+        ans = list(ans)
+        ans.sort()
+        return ans
+
+    def open_source(self, parameter_level: str, cal_date: str, number: int = 0) -> xr.DataArray:
+        parameter, level = Param.separate_FactorLevel(parameter_level)
+        grib_path = ReforecastGrib_OnTheFly_FileConfig.factor_filepath(data_center=self.__data_center,
+                                                                       init_date=cal_date,
+                                                                       number=number,
+                                                                       parameter=parameter,
+                                                                       level=level)
+        if os.path.exists(grib_path):
+            data = xr.open_dataarray(grib_path, engine='cfgrib')
+            data.name = parameter_level  # 统一命名 parameter + level
+
+            for name in ['meanSea', 'heightAboveGround', 'surface', 'entireAtmosphere', 'isobaricInhPa']:
+                if name in data.coords.keys():
+                    data = data.drop(name)
+
+            return data
+
+    def post_process(self, dataarray: xr.DataArray, parameter: str):
+        """这里没有逐6小时预报的数据处理成为逐24小时，每个中心资料脚本中继承该类，重写这个方法，把所有数据处理成逐24小时"""
+        return dataarray
+
+    def load(self, parameter_level: str, cal_date: str, number: int = 0) -> xr.DataArray:
+        data = self.open_source(parameter_level=parameter_level,
+                                cal_date=cal_date,
+                                number=number)
+        if data is not None:
+            parameter = Param.separate_FactorLevel(parameter_level)[0]
+
+            # 后处理，逐6小时预报的数据处理成为逐24小时
+            data = self.post_process(dataarray=data, parameter=parameter)
+
+            if parameter in ('t', '2t', 'wtmp', 'mn2t6', 'mx2t6'):
+                data = data - 273.15  # 温度转换
+
+        return data
 
 
 if __name__ == "__main__":
