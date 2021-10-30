8b8a356c6d857625fdc2ddbbb776e291a8feb217
leonhwb <972744806@qq.com>
2021-10-30 14:34:09 +0800

    导入数据检查读取器

--- __init__.py
+++ __init__.py
@@ -1,2 +1,2 @@
-from .s2s_dataloader.s2s_download import S2SRealtimeDownloader, S2SReforeacstDownloader, Param
+from .s2s_dataloader.s2s_download import S2SRealtimeDownloader, S2SReforeacstDownloader, Param, DownloadChecker
 from .s2s_dataloader import S2SRealtime, S2SRealtimeBase, S2SReforecast
