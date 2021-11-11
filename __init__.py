f50981d8e9cdf445c4b03d067f7b70b09ce3b860
leonhwb <972744806@qq.com>
2021-11-11 11:33:40 +0800

    save

--- __init__.py
+++ __init__.py
@@ -1,2 +1,5 @@
 from .s2s_dataloader.s2s_download import S2SRealtimeDownloader, S2SReforecastDownloader, Param, DownloadChecker
 from .s2s_dataloader import S2SRealtime, S2SRealtimeBase, S2SReforecast
+
+__all__ = ["S2SRealtimeDownloader", "S2SReforecast", "Param", "DownloadChecker", "S2SReforecastDownloader",
+           "S2SRealtime", "S2SRealtimeBase", "S2SReforecast"]
